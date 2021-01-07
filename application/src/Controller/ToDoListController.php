260bbcba60a41e01f0f56c808456216ab5d59331
amir shadanfar <a.shadanfar.it@gmail.com>
2021-01-07 16:12:31 +0300

    crud operation's method complete

--- application/src/Controller/ToDoListController.php
+++ application/src/Controller/ToDoListController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,15 +15,31 @@ class ToDoListController extends AbstractController
      */
     public function home(): Response
     {
-        return $this->render('index.html.twig');
+        $tasks = $this->getDoctrine()->getRepository(Task::class)->findBy([], ['id' => 'DESC']);
+
+        return $this->render('index.html.twig', [
+            'tasks' => $tasks
+        ]);
     }
 
     /**
      * @Route("/create", name="create_task",methods={"POST"})
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
-        exit('create');
+        $title = trim($request->request->get('title'));
+        if (empty($title))
+            return $this->redirectToRoute('home');
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = new Task;
+        $task->setTitle($title);
+
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('home');
     }
 
     /**
@@ -29,7 +47,14 @@ class ToDoListController extends AbstractController
      */
     public function switchStatus($id): Response
     {
-        exit('switchStatus ' . $id);
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = $this->getDoctrine()->getRepository(Task::class)->find($id);
+        $task->setStatus(!$task->getStatus());
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('home');
     }
 
     /**
@@ -37,6 +62,15 @@ class ToDoListController extends AbstractController
      */
     public function delete($id): Response
     {
-        exit('delete ' . $id);
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = $entityManager->getRepository(Task::class)->find($id);
+        if (!$task)
+            throw  $this->createNotFoundException('Task cant find with id = ' . $id);
+
+        $entityManager->remove($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('home');
     }
 }
